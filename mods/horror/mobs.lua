f69546733ea0874d9dbe415f9ab05ede274fba44
kroryan <adrylanube@gmail.com>
2026-08-16 22:43:25 +0200

    Fix severe lag from horror mod (deprecated add_particlespawner log spam)

--- mods/horror/mobs.lua
+++ mods/horror/mobs.lua
@@ -928,22 +928,22 @@ mobs:register_mob("horror:shadow", {
    view_range = 30,
    do_custom = function(self)
    local apos = self.object:getpos()
-		minetest.add_particlespawner(
-			1, --amount
-			0.3, --time
-			{x=apos.x-0.3, y=apos.y+0.3, z=apos.z-0.3}, --minpos
-			{x=apos.x+0.3, y=apos.y+0.3, z=apos.z+0.3}, --maxpos
-			{x=-0, y=-0, z=-0}, --minvel
-			{x=0, y=0, z=0}, --maxvel
-			{x=0,y=1,z=0}, --minacc
-			{x=0.5,y=1.2,z=0.5}, --maxacc
-			3, --minexptime
-			5, --maxexptime
-			2, --minsize
-			3, --maxsize
-			false, --collisiondetection
-			"horror_shadow.png" --texture
-		)
+		minetest.add_particlespawner({
+			amount = 1,
+			time = 0.3,
+			minpos = {x=apos.x-0.3, y=apos.y+0.3, z=apos.z-0.3},
+			maxpos = {x=apos.x+0.3, y=apos.y+0.3, z=apos.z+0.3},
+			minvel = {x=-0, y=-0, z=-0},
+			maxvel = {x=0, y=0, z=0},
+			minacc = {x=0,y=1,z=0},
+			maxacc = {x=0.5,y=1.2,z=0.5},
+			minexptime = 3,
+			maxexptime = 5,
+			minsize = 2,
+			maxsize = 3,
+			collisiondetection = false,
+			texture = "horror_shadow.png"
+		})
    end,
    animation = {
       speed_normal = 5,
